83a1715933263512eef9bb48d1d7b733d6133493
mama-ka-bah <kmahamadou858@gmail.com>
2024-02-05 10:15:50 -0100

    manage vectotrs-icon and theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import { Text, View } from "react-native";
+import colors from "./src/themes/colors";
+import fonts from "./src/themes/fonts";
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 export default function App() {
   return (
     <View style={{flex:1, alignItems:"center", justifyContent: "center"}}>
-      <Text>Hi</Text>
+      <Text style={{color: colors.primary, fontSize:fonts.size.xlg}}>Hello world</Text>
+      <AntDesign name="caretleft" size={25}/>
     </View>
   );
 }
---
+++ src/themes/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#405de6',
+    white: '#fff',
+    black: '#000',
+    grey: '#cccccc'
+};
+
+export default colors;
